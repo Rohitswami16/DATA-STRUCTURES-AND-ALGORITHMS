403895681a97ae5ea02629f1b97459f53d1566da
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2024-10-29 21:42:29 +0530

    Update 06_Insertionsort2.java

--- 06_Insertionsort2.java
+++ 06_Insertionsort2.java
@@ -3,17 +3,14 @@ package SORTING_ALGO;
 public class insertionsort2 {
 	public static void main(String[] args) {
 		int arr[] = {86, 42, 23, 18, 25};
-		
 		for(int i: InsertionSort(arr)) {
 			System.out.print(i+" ");
 		}
 	} 
 
 	public static int[] InsertionSort(int[] arr) {
-		
 		for(int i=1; i<=arr.length-1; i++) {
 			int item = arr[i];
-			
 			int j=i-1;
 			while(j >= 0 && arr[j]> item) {
 				arr[j+1] = arr[j];
