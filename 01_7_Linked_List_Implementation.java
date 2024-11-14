1dacf23630851e0d595ca2c84ca143bc895b4144
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2024-11-14 19:46:21 +0530

    Linked List Implementation

--- 01_7_Linked_List_Implementation.java
+++ 01_7_Linked_List_Implementation.java
@@ -1,3 +1,17 @@
+/**
+ * LinkedLIST Class - Demonstrates Various Ways to Traverse a LinkedList
+ * 
+ * This class provides an example of using a LinkedList in Java.
+ * It showcases how to add elements to the list and different ways to iterate over the list:
+ * - Directly printing the list
+ * - Using a traditional `for` loop
+ * - Using a `for-each` loop
+ * - Using an `Iterator`
+ * 
+ * LinkedList is a part of Java's Collection framework and allows dynamic manipulation of elements,
+ * making it ideal for applications that require frequent insertions and deletions.
+ */
+
 package Collection_LIST_BASED;
 
 import java.util.Iterator;
@@ -8,7 +22,7 @@ public class LinkedLIST {
         
         // Creating a LinkedList object
         LinkedList ll = new LinkedList();
-        
+
         // Adding elements to the LinkedList
         ll.add(100);
         ll.add(150);
@@ -17,11 +31,11 @@ public class LinkedLIST {
         ll.add(300);
         
         // Printing the LinkedList directly
-        System.out.println("\njust using print");
-        System.out.println(ll);
-        
+        System.out.println("\nDirectly printing the LinkedList:");
+        System.out.println(ll);  // Prints the list in one line
+
         // Iterating over the LinkedList using a traditional for loop
-        System.out.println("\nby using for loop");
+        System.out.println("\nUsing a traditional for loop:");
         for (int i = 0; i <= ll.size() - 1; i++) {
             System.out.print(ll.get(i) + " "); // Accessing elements by index
         }
@@ -29,7 +43,7 @@ public class LinkedLIST {
         System.out.println();
         
         // Iterating over the LinkedList using a for-each loop
-        System.out.println("\nby using for each loop");
+        System.out.println("\nUsing a for-each loop:");
         for (Object x : ll) {
             System.out.print(x + " "); // Accessing each element directly
         }
@@ -37,14 +51,10 @@ public class LinkedLIST {
         System.out.println();
         
         // Iterating over the LinkedList using an iterator
-        System.out.println("\nBy using iterator");
+        System.out.println("\nUsing an iterator:");
         Iterator it = ll.iterator();
         while (it.hasNext()) {
             System.out.print(it.next() + " "); // Using hasNext() and next() methods to iterate
         }
-
     }
 }
-
-
-
